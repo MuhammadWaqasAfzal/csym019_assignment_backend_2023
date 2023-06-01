d1a16ea323eb6f1b84f7306442e352b178207849
waqas <waqasafzal1313@gmail.com>
2023-06-02 00:05:34 +0100

    update user name and password api

---
+++ editUserName.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(0);
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: access");
+
+include('utility.php');
+
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($requestMethod == "POST") {
+    $inputData = json_decode(file_get_contents("php://input"), true);
+    
+    if (empty($inputData)) {
+        $res = editUserName($_POST);
+    } else {
+        $res = editUserName($inputData);
+    }
+
+    echo $res;
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod, " Method Not Allowed",
+    ];
+
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
--- utility.php
+++ utility.php
@@ -11,7 +11,7 @@ function registerUser($userData)
     $password =  $userData['password'];
     $confirmPassowrd = $userData['confirmPassword'];
 
-   
+
     if (empty(trim($userName))) {
         return response("422", "HTTP/1.0 422 User name is required", "User name is required");
     } else if (empty(trim($email))) {
@@ -22,8 +22,7 @@ function registerUser($userData)
         return response("422", "HTTP/1.0 422 Confirm password is required", "Confirm password is required");
     } else if ((trim($password)) !== trim($confirmPassowrd)) {
         return response("422", "HTTP/1.0 422 Passwords do not match", "Passwords do not match");
-    }
-    else {
+    } else {
         $userExits = "SELECT * FROM users WHERE email = '$email'";
         $query_run = $conn->query($userExits);
         if (mysqli_num_rows($query_run) > 0) {
@@ -36,7 +35,7 @@ function registerUser($userData)
         } else {
             $query = "INSERT INTO users (userName,password,email)
             VALUES ('$userName','$password','$email')";
-            $query_run = mysqli_query($conn, $query);           
+            $query_run = mysqli_query($conn, $query);
             if ($query_run) {
                 $query = "SELECT * FROM Users WHERE email = '$email' AND password = '$password'";
                 // $result = $conn -> query($userExits);
@@ -117,18 +116,26 @@ function resetPassword($userData)
     } else if (empty(trim($confirmPassowrd))) {
         return response("422", "HTTP/1.0 422 Confirm password is required", "Confirm password is required");
     } else if ((trim($password)) !== trim($confirmPassowrd)) {
+
         return response("422", "HTTP/1.0 422 Passwords do not match", "Passwords do not match");
     } else {
-        $userExits = "SELECT * FROM Users WHERE email = '$email'";
+
+        $userExits = "SELECT * FROM users WHERE email = '$email'";
+
         $query_run = $conn->query($userExits);
+
         if (mysqli_num_rows($query_run) > 0) {
+
             $query_run = null;
-            $query = "UPDATE `Users` SET `Password`='$password' WHERE Email = '$email'";
+
+            $query = "UPDATE `users` SET password='$password' WHERE email = '$email'";
+
             $query_run = mysqli_query($conn, $query);
+
             if ($query_run) {
-                return response("200", "Password Updated Successfully", "Password Updated Successfully");
+                return response("200", "Password Updated Successfully", "HTTP/1.0 Password Updated Successfully");
             } else {
-                return response("500", "Internal Server Error", "Internal Server Error");
+                return response("500", "Internal Server Error", "HTTP/1.0 Internal Server Error");
             }
         } else {
             return response("404", "HTTP/1.0 404 Invalid Email", "Invalid Email");
@@ -138,14 +145,14 @@ function resetPassword($userData)
 
 function logOutUser($courseData)
 {
-    
+
     global $conn;
-   
+
     $email = mysqli_real_escape_string($conn, $courseData['email']);
-   
+
     if (empty(trim($email))) {
         return response("422", "HTTP/1.0 422 Email is required", "Email is required");
-    }else {
+    } else {
         $userExits = "SELECT * FROM users WHERE email = '$email'";
         $query_run = $conn->query($userExits);
         if (mysqli_num_rows($query_run) > 0) {
@@ -175,13 +182,13 @@ function logOutUser($courseData)
 
 function deleteCourse($courseData)
 {
-    
+
     global $conn;
     $courseId = mysqli_real_escape_string($conn, $courseData['course_id']);
-   
+
     if (empty(trim($courseId))) {
         return response("422", "HTTP/1.0 422 Email is required", "Course Id is required");
-    }else {
+    } else {
         $courseExits = "DELETE  FROM fees WHERE course_id = '$courseId'";
         $query_run = $conn->query($courseExits);
         $courseExits = "DELETE  FROM modules WHERE course_id = '$courseId'";
@@ -191,13 +198,10 @@ function deleteCourse($courseData)
             $query_run = $conn->query($deleteCourse);
             if ($query_run) {
                 return response("200", "HTTP/1.0 200 Course Deleted Successfully", "Course Deleted Successfully");
-            }
-            else{
+            } else {
                 return response("500", "HTTP/1.0 500 Internal Server Error", "Internal Server Error");
-
             }
-        }
-       else {
+        } else {
             return response("404", "HTTP/1.0 404 Invalid Course Id", "Invalid Course Id");
         }
     }
@@ -206,7 +210,7 @@ function deleteCourse($courseData)
 function addCourse($courseData)
 {
     global $conn;
-  
+
     $title = mysqli_real_escape_string($conn, $courseData['title']);
     $level = mysqli_real_escape_string($conn, $courseData['level']);
     $full_time_duration = mysqli_real_escape_string($conn, $courseData['full_time_duration']);
@@ -215,14 +219,14 @@ function addCourse($courseData)
     $part_time_duration = mysqli_real_escape_string($conn, $courseData['part_time_duration']);
     $start = ($courseData['start']);
     $location = mysqli_real_escape_string($conn, $courseData['location']);
-    $overview = mysqli_real_escape_string($conn, $courseData['overview']); 
+    $overview = mysqli_real_escape_string($conn, $courseData['overview']);
     $modules = ($courseData['modules']);
     $fees = ($courseData['fees']);
-    $entryRequirements = mysqli_real_escape_string($conn,($courseData['entryRequirements']));
+    $entryRequirements = mysqli_real_escape_string($conn, ($courseData['entryRequirements']));
 
-    
-  
-    if(empty(trim($title))) {
+
+
+    if (empty(trim($title))) {
         return response("422", "HTTP/1.0 422 Course title is required", "Course title is required");
     } else if (empty(trim($level))) {
         return response("422", "HTTP/1.0 422 Course level cannot be empty", "Course level cannot be empty");
@@ -240,20 +244,19 @@ function addCourse($courseData)
         return response("422", "HTTP/1.0 422 Course location is required", "Course location is required");
     } else if (empty(trim($overview))) {
         return response("422", "HTTP/1.0 422 Course overview is required", "Course overview is required");
-    }
-    else {
+    } else {
 
-            $query = "INSERT INTO `courses`(`title`, `full_time_duration`, `full_time_with_placement_duration`,
+        $query = "INSERT INTO `courses`(`title`, `full_time_duration`, `full_time_with_placement_duration`,
              `full_time_foundation_duration`, `part_time_duration`, `start`, `location`, `overview`, `level`,`entry_requirements`) 
-                    VALUES ('$title','$full_time_duration','$full_time_with_placement_duration','$full_time_foundation_duration','$part_time_duration','$start','$location','$overview','$level','$entryRequirements')";          
-          
-           
-           try {
-           $query_run = mysqli_query($conn, $query);
+                    VALUES ('$title','$full_time_duration','$full_time_with_placement_duration','$full_time_foundation_duration','$part_time_duration','$start','$location','$overview','$level','$entryRequirements')";
+
+
+        try {
+            $query_run = mysqli_query($conn, $query);
             if ($query_run) {
-               
+
                 //foreign key for modules and fees tables
-                $lastinnsertedId=strval(mysqli_insert_id($conn));
+                $lastinnsertedId = strval(mysqli_insert_id($conn));
                 $values = array();
                 foreach ($modules as $item) {
                     $values[] = "('{$lastinnsertedId}','{$item['category']}','{$item['name']}','{$item['credit_hours']}','{$item['code']}','{$item['status']}','{$item['pre_requisites']}')";
@@ -262,59 +265,55 @@ function addCourse($courseData)
                 $values = implode(", ", $values);
                 $query = "INSERT INTO `modules`(`course_id`,`category`, `name`, `credit_hours`, `code`, `status`, `pre_requisites`) VALUES {$values}";
                 $query_run = mysqli_query($conn, $query);
-               
-               
+
+
                 if ($query_run) {
-                  
+
                     $feeValues = array();
                     foreach ($fees as $item) {
                         $feeValues[] = "('{$lastinnsertedId}','{$item['session']}','{$item['uk_full_time_fee']}','{$item['uk_part_time_fee']}','{$item['uk_part_time_year']}','{$item['uk_part_time_per_credit_hour']}','{$item['uk_part_time_total_credit_hours']}','{$item['uk_international_foundation_year']}','{$item['international_full_year_fee']}','{$item['international_integrated_foundation_year_fee']}','{$item['placement_fee']}','{$item['additional_cost']}')";
                     }
-    
+
                     $feeValues = implode(", ", $feeValues);
                     $query = "INSERT INTO `fees` (`course_id`, `session`, `uk_full_time_fee`, `uk_part_time_fee`,`uk_part_time_year`,`uk_part_time_per_credit_hour`,`uk_part_time_total_credit_hours`, `uk_international_foundation_year`, `international_full_year_fee`,`international_integrated_foundation_year_fee`, `placement_fee`, `additional_cost`) VALUES {$feeValues}";
-                   
+
                     $query_run = mysqli_query($conn, $query);
-                 
+
                     if ($query_run) {
                         return response("201", "HTTP/1.0 201 Course added Successfully", "Course added Successfully");
-                    }else{
+                    } else {
                         return response("500", "Internal Server Error", "HTTP/1.0 500 Internal Server Error");
                     }
-                }else{                
+                } else {
                     return response("500", "Internal Server Error", "HTTP/1.0 500 Internal Server Error");
-                }           
+                }
             } else {
                 return response("500", "Internal Server Error", "HTTP/1.0 500 Internal Server Error");
             }
-        }catch (mysqli_sql_exception $e) {
+        } catch (mysqli_sql_exception $e) {
             // Handle the unique constraint error
             if ($e->getCode() === 1062) {
                 return response("1062", "Course Name Already Exists.", "HTTP/1.0 1062 Course Name Already Exists.");
 
-              // Additional error handling or actions
+                // Additional error handling or actions
             } else {
-              // Handle other types of exceptions or errors
-              echo "An error occurred: " . $e->getMessage();
+                // Handle other types of exceptions or errors
+                echo "An error occurred: " . $e->getMessage();
             }
-          }
-       
+        }
     }
-    
-    
 }
 
 
-function updateCourse($courseData){
+function updateCourse($courseData)
+{
     global $conn;
-  
+
     $courseId = mysqli_real_escape_string($conn, $courseData['id']);
 
-    if(empty(trim($courseId))) {
+    if (empty(trim($courseId))) {
         return response("422", "HTTP/1.0 422 Course Id is required", "Course Id is required");
-    }
-    else
-    {
+    } else {
 
         $deleteModules = "DELETE FROM `modules` WHERE  Id = '$courseId'";
         $query_run = $conn->query($deleteModules);
@@ -323,17 +322,15 @@ function updateCourse($courseData){
         $query_run = $conn->query($deleteFees);
 
 
-            $deleteCourse = "DELETE FROM `courses` WHERE  Id = '$courseId'";
-            $query_run = $conn->query($deleteCourse);
-            if ($query_run) {
-                addCourse($courseData);
-                return response("200", "HTTP/1.0 200 Course Updated Successfully", "Course Updated Successfully");
-            }
-            else{
-                return response("500", "HTTP/1.0 500 Internal Server Error", "Internal Server Error");
-
-            }
-     }
+        $deleteCourse = "DELETE FROM `courses` WHERE  Id = '$courseId'";
+        $query_run = $conn->query($deleteCourse);
+        if ($query_run) {
+            addCourse($courseData);
+            return response("200", "HTTP/1.0 200 Course Updated Successfully", "Course Updated Successfully");
+        } else {
+            return response("500", "HTTP/1.0 500 Internal Server Error", "Internal Server Error");
+        }
+    }
 }
 
 function getAllCourses($data)
@@ -344,20 +341,20 @@ function getAllCourses($data)
 
     if ($query_run) {
         $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
-       
+
         for ($x = 0; $x < count($res); $x++) {
             $row = $res[$x];
             $courseId = $row["Id"];
             $query = "SELECT * FROM modules where course_id = '$courseId'";
             $query_run = mysqli_query($conn, $query);
             $modulesRes = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
-            $res[$x]["modules"] = $modulesRes; 
+            $res[$x]["modules"] = $modulesRes;
             $query = "SELECT * FROM fees where course_id = '$courseId'";
             $query_run = mysqli_query($conn, $query);
             $feesRes = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
-            $res[$x]["fees"] = $feesRes; 
+            $res[$x]["fees"] = $feesRes;
         }
-        
+
         $data = [
             'status' => 200,
             'message' => "All Courses",
@@ -381,4 +378,39 @@ function response($statusCode, $header, $message)
     header($header);
     echo json_encode($data);
     exit();
+}
+
+
+function editUserName($userData)
+{
+    global $conn;
+
+    $email = mysqli_real_escape_string($conn, $userData['email']);
+    $userName = mysqli_real_escape_string($conn, $userData['userName']);
+    $password = mysqli_real_escape_string($conn, $userData['password']);
+
+   
+
+    if (empty(trim($userName))) {
+        return response("422", "HTTP/1.0 422 User name is required", " User name is required");
+    }else if (empty(trim($email))) {
+        return response("422", "HTTP/1.0 422 Email is required", "Email is required");
+    } else {
+        $query = "UPDATE `users` SET `userName`='$userName' , `password`='$password' WHERE email = '$email'";
+        $query_run = mysqli_query($conn, $query);
+        if ($query_run) {
+            $query = "SELECT * FROM Users WHERE email = '$email'";
+            $result = mysqli_query($conn, $query);
+            $res = mysqli_fetch_assoc($result);
+            $data = [
+                'status' => 200,
+                'message' => "User name updated Successfully",
+                'data' => $res
+            ];
+            header("HTTP/1.0 200 Success");
+            return json_encode($data);
+        } else {
+            return response("500", "Internal Server Error", "HTTP/1.0 500 Internal Server Error");
+        }
+    }
 }
